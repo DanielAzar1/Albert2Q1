73276696b0294e414cff21d3e9c67f8072a12e9b
Daniel azar <da6331@bs.amalnet.k12.il>
2023-12-19 09:44:43 +0200

    Initial commit

---
+++ app/src/main/java/com/example/albert2q1/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.albert2q1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button btn;
+    TextView txtV;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        btn = findViewById(R.id.btn);
+        txtV = findViewById(R.id.txtV);
+    }
+
+    public void lol(View view) {
+        btn = findViewById(R.id.btn);
+        txtV.setText("oh yeah ive been clicked!");
+    }
+}
